25b2f1d2d9697e4b2b3db5025444faf0cef30393
publicsbk <shebeko_pavel@mail.ru>
2022-02-03 18:23:36 +0300

    5. Конвертер валюты [#41583]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -2,12 +2,12 @@ package ru.job4j.converter;
 
 public class Converter {
     public static float rubleToEuro(float value) {
-        float rsl = value / 70; // 1 евро = 70 рублей.
+        float rsl = value / 70;
         return rsl;
     }
 
     public static float rubleToDollar(float value) {
-        float rsl = value / 60; // 1 доллар = 60 рублей.
+        float rsl = value / 60;
         return rsl;
     }
 
