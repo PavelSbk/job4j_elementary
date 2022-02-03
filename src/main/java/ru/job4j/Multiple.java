968085d26611a6f2f75f95db62938136d43a5e66
publicsbk <shebeko_pavel@mail.ru>
2022-02-03 15:15:02 +0300

    3.3. Фиксация изменений на github [#243564 #259817]

---
+++ src/main/java/ru/job4j/Multiple.java
@@ -0,0 +1,14 @@
+package ru.job4j;
+
+public class Multiple {
+    public static void main(String[] args) {
+        System.out.println("1 * 2 = 2");
+        System.out.println("1 * 2 = 3");
+        System.out.println("1 * 2 = 4");
+        System.out.println("1 * 2 = 5");
+        System.out.println("1 * 2 = 6");
+        System.out.println("1 * 2 = 7");
+        System.out.println("1 * 2 = 8");
+        System.out.println("1 * 2 = 9");
+    }
+}
