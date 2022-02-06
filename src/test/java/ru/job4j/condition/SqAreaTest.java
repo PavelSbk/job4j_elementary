3a705af4967e208db94be37f254bf563a4e1d8f2
publicsbk <shebeko_pavel@mail.ru>
2022-02-06 13:56:02 +0300

    8. Стороны прямоугольника. [#137735]

---
+++ src/test/java/ru/job4j/condition/SqAreaTest.java
@@ -0,0 +1,19 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SqAreaTest {
+    /*
+    public static double square(double p, double k) {
+    double h = p / (2 * (k + 1));
+    double l = h * k;
+    return l * h;
+     */
+
+    @Test
+    public void square() {
+
+    }
+}
