82bbc4b7afc258d6f690b67c9c54cc6c5c778bda
publicsbk <shebeko_pavel@mail.ru>
2022-02-14 00:28:00 +0300

    6.0.2. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,10 +5,21 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surname = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
 
-        System.out.println("Размер массива 'ages' равен: " + ages.length);
+        System.out.println("\nРазмер массива 'ages' равен: " + ages.length);
         System.out.println("Размер массива 'surname' равен: " + surname.length);
         System.out.println("Размер массива 'prices' равен: " + prices.length);
-    }
+        System.out.println();
 
+        names[0] = "Ivan Ivanov";
+        names[1] = "Oleg Kozlov";
+        names[2] = "Irina Ivanova";
+        names[3] = "Olga Kozlova";
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
+    }
 }
+
