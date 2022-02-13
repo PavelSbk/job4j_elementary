99eb408e6224e21eb82454c57ec104f56a82a5d5
publicsbk <shebeko_pavel@mail.ru>
2022-02-13 23:48:35 +0300

    5.7. Ипотека [#156315 #263761]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -4,7 +4,7 @@ public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
         percent /= 100;
-        while (amount >= 0) {
+        while (amount > 0) {
             amount = (amount * percent + amount) - salary;
             year++;
         }
