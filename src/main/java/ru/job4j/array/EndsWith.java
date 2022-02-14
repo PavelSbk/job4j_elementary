83a72b1e3332b84c5b2e15462cb61cec953ba1c4
publicsbk <shebeko_pavel@mail.ru>
2022-02-14 23:00:47 +0300

    6.4.2. Слово заканчивается на ... [#156317]

---
+++ src/main/java/ru/job4j/array/EndsWith.java
@@ -0,0 +1,15 @@
+package ru.job4j.array;
+
+public class EndsWith {
+    public static boolean endsWith(char[] word, char[] post) {
+        boolean result = false;
+        for (int i = word.length - 1, j = post.length - 1; i > post.length; i--, j--) {
+            if (word[i] == post[j]) {
+                result = true;
+            } else {
+                break;
+            }
+        }
+        return result;
+    }
+}
