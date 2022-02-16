ebaafddf96d3c944b97b0b12154e0e572bbcc365
publicsbk <shebeko_pavel@mail.ru>
2022-02-16 17:38:03 +0300

    6.7.1. Моно строка в матрице. [#214126 #264939]

--- src/test/java/ru/job4j/array/MatrixCheckTest.java
+++ src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -22,9 +22,9 @@ public class MatrixCheckTest {
     @Test
     public void whenHasNotMonoHorizontal() {
         char[][] input = {
+                {' ', ' ', 'X'},
                 {' ', ' ', ' '},
-                {' ', ' ', ' '},
-                {' ', ' ', ' '},
+                {'X', ' ', 'X'},
         };
         int row = 1;
         boolean result = MatrixCheck.monoHorizontal(input, row);
